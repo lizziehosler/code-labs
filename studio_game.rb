d50c849e062abeb30c5ba0458e6aa4572454b1a2
lizziehosler <lizzie.hosler@gmail.com>
2020-07-12 18:15:06 -0500

    classes and attributes

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,49 @@
-def say_hello(name, health=100)
-    "I'm #{name.capitalize} with a health of #{health} as of #{time}."
+class Player
+    attr_reader :health
+    attr_accessor :name
+    def score
+        @health + @name.length
+    end
+
+    def name=(new_name)
+        @name = new_name.capitalize
+    end
+
+    def initialize(name, health=100)
+        @name = name.capitalize
+        @health = health
+    end
+
+    def to_s
+        "I'm #{@name} with a health of #{@health} and a score of #{score}."
+    end
+
+    def blam
+        @health -= 10
+        puts "#{@name} got blammed!"
+    end
+
+    def w00t
+        @health += 15
+        puts "#{@name} got w00ted!"
+    end
+
 end
 
-def time
-    Time.new.strftime("%I:%M:%S")
-end
+player1 = Player.new("moe")
+puts player1
+player1.w00t
+puts player1
 
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
+player2 = Player.new("larry", 60)
+
+
+player3 = Player.new("curly", 125)
+puts player3
+player3.blam
+puts player3
+player3.w00t
+puts player3
+
+player2.name = "lawrence"
+puts player2.name
